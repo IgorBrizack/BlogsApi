732b1bcf306a1ea0ed4bd5a729d49c922f75f66a
Igor Brizack <igor_brizack@hotmail.com>
2022-09-23 20:20:31 -0300

    requisito 4 concluido

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,11 +5,11 @@ const insertUserController = async (req, res) => {
   const userData = req.body;
   const data = await UserService.insertUserService(userData);
 
-  if (data === 'unique violation') res.status(409).json({ message: 'User already registered' });
+  if (!data) return res.status(409).json({ message: 'User already registered' });
 
   const token = generateToken.generateUserToken(data);
 
-  res.status(200).json({ token });
+  res.status(201).json({ token });
 };
 
 module.exports = {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,12 +1,12 @@
 const { User } = require('../models');
 
 const insertUserService = async ({ displayName, email, password, image }) => {
+  const isDuplicated = await User.findOne({ where: { email } });
+ 
+  if (isDuplicated) return false;
+  
   const usersData = await User.create({ displayName, email, password, image });
-
-  console.log(usersData);
-  if (usersData.errors[0].type === 'unique violation') {
-    return usersData.errors[0].type;
-  }
+  
   return usersData;
 };
 
