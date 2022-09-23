fdca9ecc7847e1f522cd187081688ab12bdab50c
Igor Brizack <igor_brizack@hotmail.com>
2022-09-23 17:05:51 -0300

    requisito 2 concluido

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('Users', {
+  const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
-    display_name: { type: DataTypes.STRING },
+    displayName: { type: DataTypes.STRING },
     email: { type: DataTypes.STRING, unique: true, allowNull:false },
     password: { type:DataTypes.STRING, allowNull: false },
     image: { type: DataTypes.INTEGER },
   },
   {
     timestamps: false,
-    tableName: 'users',
+    tableName: 'User',
     underscored: true,
   });
 
-  return Users;
+  return User;
 };
